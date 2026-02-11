4875f52992f36d38dda47f5e16e3a85955f5df16
Salvador Ursua <118657557+SalvadorUrsua@users.noreply.github.com>
2026-02-11 19:06:12 +0800

    Add files via upload

--- js/smu.js
+++ js/smu.js
@@ -2,9 +2,9 @@ const currentYear = new Date().getFullYear();
 year = document.querySelector(".year");
 year.textContent = currentYear;
 
-const btrNavEl = document.querySelector(".btn-mobile-nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-btrNavEl.addEventListener("click", () => {
+btnNavEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
 });
 
@@ -26,6 +26,38 @@ anchorLinks.forEach(function (link) {
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
+      const btns = document.querySelectorAll("a");
+      btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          btn.blur();
+        });
+      });
     }
   });
 });
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const options = {
+  root: null, // to observe the element in the viewport / screen / browser window
+  threshold: 0, // when the element is already outside the screen / viewport / browser
+};
+
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  const mainNav = document.querySelector(".header");
+  if (!ent.isIntersecting) {
+    mainNav.classList.toggle("sticky");
+    document.querySelector(".section-hero").classList.add("observer");
+  } else {
+    mainNav.classList.remove("sticky");
+    document.querySelector(".section-hero").classList.remove("observer");
+  }
+}, options);
+
+obs.observe(sectionHeroEl);
+
+// const obs = new IntersectionObserver(function observerFunction(entries) {
+//   const ent = entries[0];
+//   console.log(ent.isIntersecting);
+// }, opts);
+// obs.observe(sectionHeroEl);
