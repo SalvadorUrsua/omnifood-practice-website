ab0a8a15e26d340c31cfcda70c9c72506a2e3377
Salvador Ursua <118657557+SalvadorUrsua@users.noreply.github.com>
2026-02-11 23:19:20 +0800

    Add files via upload

--- js/smu.js
+++ js/smu.js
@@ -37,22 +37,42 @@ anchorLinks.forEach(function (link) {
 });
 
 const sectionHeroEl = document.querySelector(".section-hero");
-const options = {
-  root: null, // to observe the element in the viewport / screen / browser window
-  threshold: 0, // when the element is already outside the screen / viewport / browser
-};
+const mobileBtn = document.querySelectorAll(".btn-mobile-nav");
+const mobileDisplay = window.getComputedStyle(mobileBtn[0]).display;
+// console.log(mobileDisplay);
+function getOptions() {
+  if (mobileDisplay === "block") {
+    // MOBILE
+    return { root: null, threshold: 0 }; //, rootMargin: "0px 0px -300px 0px"
+  } else {
+    // LARGE SCREEN
+    return { root: null, threshold: 0 };
+  }
+}
+
+// const options = {
+//   root: null, // to observe the element in the viewport / screen / browser window
+//   threshold: 0, // when the element is already outside the screen / viewport / browser
+// };
 
 const obs = new IntersectionObserver(function (entries) {
   const ent = entries[0];
   const mainNav = document.querySelector(".header");
+  // console.log(ent.isIntersecting);
   if (!ent.isIntersecting) {
     mainNav.classList.toggle("sticky");
     document.querySelector(".section-hero").classList.add("observer");
+    // if (mobileDisplay === "block") {
+    //   document.querySelector(".section-hero").classList.add("observer-sm");
+    // } else {
+    //   document.querySelector(".section-hero").classList.add("observer");
+    // }
   } else {
     mainNav.classList.remove("sticky");
     document.querySelector(".section-hero").classList.remove("observer");
+    // document.querySelector(".section-hero").classList.remove("observer-sm");
   }
-}, options);
+}, getOptions());
 
 obs.observe(sectionHeroEl);
 
